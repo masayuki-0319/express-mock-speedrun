a1282a3add481107f77db9183a233bf47427d8e3
将之大西 <omccello@gmail.com>
2021-09-25 00:54:56 +0900

    fix linter

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import {json, urlencoded} from "body-parser";
+import express from 'express';
+import { json, urlencoded } from 'body-parser';
 
 const app = express();
 const router = express.Router();
@@ -8,16 +8,16 @@ app.use(json());
 app.use(urlencoded({ extended: false }));
 
 router.get(
-  "/",
+  '/',
   async (req: express.Request, res: express.Response): Promise<void> => {
-    res.send("hello world");
+    res.send('hello world');
   }
 );
 
-app.use("/", router);
+app.use('/', router);
 
 app.listen(3000, () => {
-  console.log("listening on port 3000");
+  console.log('listening on port 3000');
 });
 
 export default app;
