11e40a8488dfc5bb34798164f5687cfefe93fe2b
将之大西 <omccello@gmail.com>
2021-09-25 15:27:07 +0900

    Setting winston

---
+++ src/config/logger/winston.ts
@@ -0,0 +1,53 @@
+import winston, { LoggerOptions } from 'winston';
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const level = () => {
+  const env = process.env.NODE_ENV || 'development';
+  const isDevelopment = env === 'development';
+
+  return isDevelopment ? 'debug' : 'info';
+};
+
+const format = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+  winston.format.colorize({ all: true }),
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}`
+  )
+);
+
+// TODO: logs ディレクトリのパスを検討すること
+const transports = [
+  new winston.transports.Console(),
+  new winston.transports.File({
+    filename: 'logs/error.log',
+    level: 'error',
+  }),
+  new winston.transports.File({ filename: 'logs/all.log' }),
+];
+
+const loggerOptions: LoggerOptions = {
+  level: level(),
+  levels,
+  format,
+  transports,
+};
+
+const levelColorOption = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'white',
+};
+
+winston.addColors(levelColorOption);
+
+export const Logger = winston.createLogger(loggerOptions);
